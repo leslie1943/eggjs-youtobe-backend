25dd16dafaf1b478a3a2c2ba86dff419d4c9c3ef
leslie <leslie43@sina.com>
2021-04-29 23:28:21 +0800

    deploy test

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,16 +2,16 @@ const Controller = require('egg').Controller
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this
-    const User = this.app.model.User
+    // const { ctx } = this
+    // const User = this.app.model.User
 
-    // 新增一个 user
-    await new User({
-      userName: 'admin',
-      password: 'admin',
-    }).save()
+    // // 新增一个 user
+    // await new User({
+    //   userName: 'admin',
+    //   password: 'admin',
+    // }).save()
 
-    ctx.body = 'Hello, egg'
+    this.ctx.body = 'Hello, Leslie'
   }
 }
 
